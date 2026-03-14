f800d1c14ca0d36175a9b7127bd513e3eb58fa5c
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-14 15:15:44 +0530

    roboshoop

---
+++ mongodb.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+USERID=$(id -u )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FLODER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FLODER/$SCRIPTE_NAME.log"
+
+mkdir -p $LOGS_FLODER
+
+echo "Script stared  executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then 
+    echo "ERROR:: Please run this acript with root privelege"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing  $2 ...$R FAILURE $N" | tee -a $LOGS_FLODER
+        exit 1
+    else
+        echo -e "Installing $2 ...$G SUCCESS $N" |tee -a $LOG_FILE
+    fi         
+}
+
+# $@
+for package in $@
+do 
+    #checck package is already installed oor not 
+    dnf list installed $package &>>$LOG_FILE
+
+    # if exit status is 0, already installed. -ne 0 need too install it
+    if [ $? -ne 0 ]; then
+        dnf install $package -y $>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "$package already installed ...$Y SKIPPING $N"
+    fi
+dne     
+
+
+cp vim /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Intalling Mongodb"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable Mongodb"
+
+systemctl start mongod 
+VALIDATE $? "Start Mongodb"
+
+
---
+++ roboshop.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+AMI_ID=""
+SG_ID=""
+ZONE_ID=""
+DOMAIN_NAME="heysushma.fun"
+
+for instance in $@  #mongodb redis mysql
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    #Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.heysushma.fun
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PubilicIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # heysushma.fun    
+fi
+
+echo "$instance: $IP"
+
+aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
+done
