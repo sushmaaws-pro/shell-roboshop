a642c1e03444b5a45ed3287f287fd06eebc1f7df
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-14 18:28:55 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -11,7 +11,6 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOlDER
-
 echo "Script stared  executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then 
