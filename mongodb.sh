e05f6bf65de5bea95d053a548b092457fe3f4a86
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-14 18:21:06 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-logs"
+LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FLODER/$SCRIPTE_NAME.log"
 
