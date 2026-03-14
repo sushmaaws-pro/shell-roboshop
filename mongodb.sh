687ba3744966d87343a462513884cd71e1637178
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-14 18:30:33 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -5,13 +5,12 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOGS_FOlDER
-echo "Script stared  executed at: $(date)" | tee -a $LOG_FILE
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then 
     echo "ERROR:: Please run this acript with root privelege"
