62a54ee49399b52f17b3b229aefbe9f04ffc9ea0
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-15 12:44:32 +0530

    Roboshop

--- catalouge-set.sh
+++ catalouge-set.sh
@@ -12,7 +12,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-SCRIPT_DIR=$pwd
+SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.heysushma.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
