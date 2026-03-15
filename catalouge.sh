4b8612af2ded40e66348baf039c7b09b90c51b1f
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-15 12:02:19 +0530

    Roboshop

--- catalouge.sh
+++ catalouge.sh
@@ -22,10 +22,10 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e $2...$R FAILURE $N | tee -a $LOG_FILE
+        echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e $2...$G SUCCESS $N |tee -a $LOG_FILE   
+        echo -e "$2...$G SUCCESS $N" |tee -a $LOG_FILE   
     fi     
 }
 
