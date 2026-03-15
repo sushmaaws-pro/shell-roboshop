ae442ff8b1ee818ef595560a3c1d2d4fd0c06431
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-15 12:49:40 +0530

    Roboshop

--- catalouge-set.sh
+++ catalouge-set.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-trap 'echo "There is an error in $LINENO, command is: $BASH_COMMAND"' ERR
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
 
 USERID=$(id -u)
 R="\e[31m"
@@ -13,25 +13,22 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.heysushma.fun
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root prevelege"
-    exit 1
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-
-####### NodeJS #####
+##### NodeJS ####
 dnf module disable nodejs -y &>>$LOG_FILE
 dnf module enable nodejs:20 -y  &>>$LOG_FILE
 dnf install nodejs -y &>>$LOG_FILE
-
-echo -e "Installing NodeJS 20...$G SUCCESS $N"
-
+echo -e "Installing NodeJS 20 ... $G SUCCESS $N"
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
@@ -42,18 +39,17 @@ fi
 
 mkdir -p /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-
 cd /app 
 rm -rf /app/*
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 npm install &>>$LOG_FILE
-cp $SCRIPT_DIR/catalogue.service/etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue &>>$LOG_FILE
-echo -e "Catalogue applicatin setup ... $G SUCCESS $N"
+echo -e "Catalogue application setup ... $G SUCCESS $N"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y &>>$LOG_FILE
+dnf install mongodb-mongoshsfds -y &>>$LOG_FILE
 
 INDEX=$(mongosh mongodb.heysushma.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
@@ -63,4 +59,4 @@ else
 fi
 
 systemctl restart catalogue
-echo -e "Loding producat and restarting catalogue ...$G SUCCESS $N"
+echo -e "Loading products and restarting catalogue ... $G SUCCESS $N"
