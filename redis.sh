aec840210fc0c183d6eb4109386a50d8a179ddb8
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-16 13:06:14 +0530

    Roboshop

--- redis.sh
+++ redis.sh
@@ -34,7 +34,7 @@ VALIDATE $? "Enabling Redis 7"
 dnf install redis -y  &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no'/etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Allowing Remote connections to Redis"
 
 systemctl enable redis &>>$LOG_FILE
