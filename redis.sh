96a49fddd976481ff077aefc3e70625f43fb08d6
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-16 13:06:56 +0530

    Roboshop

--- redis.sh
+++ redis.sh
@@ -44,5 +44,5 @@ VALIDATE $? "Starting Redis"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME))
-echo "Script executed in: $Y $TOTAL_TIME Seconds $N"
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
 
