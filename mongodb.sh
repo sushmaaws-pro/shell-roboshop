3d6be66bd50591434ea2e74cbd9f51fcd3f859a1
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-16 11:18:52 +0530

    Roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -59,4 +59,4 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections to MongoDB"
 
 systemctl restart mongod
-VALIDATE $? "Restarted MongoDB"
+VALIDATE $? "Restarted MongoDB
