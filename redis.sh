18535829eff57f9d9ded1ee478819bad21855690
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-16 12:59:39 +0530

    Roboshop

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOOLDER/$SCRIPT_NAME.LOG"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.LOG"
 START_TIME=$(date +%s)
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
